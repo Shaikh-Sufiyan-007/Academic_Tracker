54db9df17f62cf8b5f90ab29ec04193daec0af54
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-07-23 13:33:53 +0530

    0 - adding dependencies and some basic setup

---
+++ api/server.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(cookieParser())
+
+// MONGODB CONNECTION
+ mongoose.connect(`mongodb://localhost:27017/academictracker`).then(db => {
+    console.log("Database is connected successfully")
+ }).catch(err => {
+     console.log("Database is not connected ", err)
+ })
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
