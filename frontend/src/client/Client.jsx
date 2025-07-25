d8e92938d72536bcf881435143254d8829e10998
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-07-25 20:37:43 +0530

    1 - creating footer of client page

--- frontend/src/client/Client.jsx
+++ frontend/src/client/Client.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './utility_components/nabar/Navbar'
+import Footer from './utility_components/footer/Footer'
+import Box from '@mui/material/Box'
 
 const Client = () => {
   return (
     <>
-        <div>
-            <Navbar />
+        <Navbar />
+        <Box sx={{minHeight: '80vh'}} component={'div'}>
             <Outlet />
-        </div>
+        </Box>
+        <Footer />
     </>
 
   )
--- frontend/src/client/utility_components/footer/Footer.jsx
+++ frontend/src/client/utility_components/footer/Footer.jsx
@@ -1,8 +1,17 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }} component={'div'}>
+
+        <Typography variant='h5'>Academic Tracker</Typography>
+        <Typography variant='p'>Copyright@2025</Typography>
+
+      </Box>
+    </>
   )
 }
 
