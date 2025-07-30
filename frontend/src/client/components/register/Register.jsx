707d30c6a43c37bc93d09c1578b61a1d3ade7469
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-07-30 19:51:54 +0530

    1 - register page frontend part

--- frontend/src/client/components/register/Register.jsx
+++ frontend/src/client/components/register/Register.jsx
@@ -1,9 +1,145 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Form, useFormik } from "formik";
+import { registerSchema } from "../../../yupSchema/registerSchema";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import CardMedia from "@mui/material/CardMedia";
+import { useRef } from "react";
+import Typography from "@mui/material/Typography";
 
-const Register = () => {
+export default function Register() {
+
+  const [file, setFile] = useState(null)
+  const [imageUrl, setImageUrl] = useState(null)
+  const addImage = (e) => {
+    const file = e.target.files[0]
+    setImageUrl(URL.createObjectURL(file))
+    setFile(file)
+  }
+
+  const fileInputRef = useRef(null)
+  const handleClearFile = () => {
+    if(fileInputRef.current){
+      fileInputRef.current.value = '';
+    }
+    setFile(null)
+    setImageUrl(null)
+  }
+
+  const initialValues = {
+    school_name: "",
+    email: "",
+    owner_name: "",
+    password: "",
+    confirm_password: "",
+  };
+  const Formik = useFormik({
+    initialValues,
+    validationSchema: registerSchema,
+    onSubmit: (values) => {
+      console.log("Register submit values: ", values);
+      Formik.resetForm();
+      handleClearFile()
+    },
+  });
+
   return (
-    <div>Register</div>
-  )
-}
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1 },
+        display: "flex",
+        flexDirection: "column",
+        width: "60vw",
+        minWidth: "230px",
+        margin: 'auto',
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={Formik.handleSubmit}
+    >
 
-export default Register
+      <Typography>Add College Picture</Typography>
+      <TextField
+      inputRef={fileInputRef}
+      type="file"
+      onChange={(e) => addImage(e)}
+      />
+      {imageUrl && <Box>
+        
+          <CardMedia component={'img'} height={'240px'} image={imageUrl} />
+        </Box>}
+
+      <TextField
+        name="school_name"
+        label="School Name"
+        value={Formik.values.school_name}
+        onChange={Formik.handleChange}
+        onBlur={Formik.handleBlur}
+      />
+      {Formik.touched.school_name && Formik.errors.school_name && (
+        <p style={{ color: "red", textTransform: "capitalize" }}>
+          {Formik.errors.school_name}
+        </p>
+      )}
+
+      <TextField
+        name="email"
+        label="Email"
+        value={Formik.values.email}
+        onChange={Formik.handleChange}
+        onBlur={Formik.handleBlur}
+      />
+      {Formik.touched.email && Formik.errors.email && (
+        <p style={{ color: "red", textTransform: "capitalize" }}>
+          {Formik.errors.email}
+        </p>
+      )}
+
+      <TextField
+        name="owner_name"
+        label="Owner Name"
+        value={Formik.values.owner_name}
+        onChange={Formik.handleChange}
+        onBlur={Formik.handleBlur}
+      />
+      {Formik.touched.owner_name && Formik.errors.owner_name && (
+        <p style={{ color: "red", textTransform: "capitalize" }}>
+          {Formik.errors.owner_name}
+        </p>
+      )}
+
+      <TextField
+        type="password"
+        name="password"
+        label="Password"
+        value={Formik.values.password}
+        onChange={Formik.handleChange}
+        onBlur={Formik.handleBlur}
+      />
+      {Formik.touched.password && Formik.errors.password && (
+        <p style={{ color: "red", textTransform: "capitalize" }}>
+          {Formik.errors.password}
+        </p>
+      )}
+
+      <TextField
+      type="password"
+        name="confirm_password"
+        label="Confirm Password"
+        value={Formik.values.confirm_password}
+        onChange={Formik.handleChange}
+        onBlur={Formik.handleBlur}
+      />
+      {Formik.touched.confirm_password && Formik.errors.confirm_password && (
+        <p style={{ color: "red", textTransform: "capitalize" }}>
+          {Formik.errors.confirm_password}
+        </p>
+      )}
+
+      <Button type="submit" variant="contained">Submit</Button>
+    </Box>
+  );
+}
--- frontend/src/yupSchema/registerSchema.js
+++ frontend/src/yupSchema/registerSchema.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const registerSchema = yup.object({
     school_name: yup.string().min(8, 'School name must contain 8 characters.').required("School name is required."),
-    email: yup.string.email("It must be an Email").required("Email is required."),
+    email: yup.string().email("It must be an Email").required("Email is required."),
     owner_name: yup.string().min(3, 'Owner name must contain 3 characters.').required("Owner name is required."),
     password: yup.string().min(8, 'Password must contain 8 characters.').required("Password is required."),
     confirm_password: yup.string().oneOf([yup.ref('password')], 'Passwords must match').required("Confirm password is required."),
